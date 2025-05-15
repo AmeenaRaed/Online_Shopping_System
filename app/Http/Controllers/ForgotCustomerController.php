b1e04e2fde65462431e1c0bda6674237b364c443
bashayeraldoseri <bashayeraldoseri421@gmail.com>
2025-05-15 22:09:20 +0300

    Update

--- app/Http/Controllers/ForgotCustomerController.php
+++ app/Http/Controllers/ForgotCustomerController.php
@@ -7,6 +7,12 @@ use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class ForgotCustomerController extends Controller{
+
+public function showForm()
+{
+    return view('registration.forgetCustomer');
+}
+    
 public function reset(Request $request)
 {
     // Validate input
